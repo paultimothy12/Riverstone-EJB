7fbba65d940102b8fbe7681d4e9acbc6fc985785
paultimothy12 <paulromerio@gmail.com>
2023-08-30 12:16:02 +0530

    MDB p2p

---
+++ DemoMDB-ejb/src/java/com/timothy/MyBean.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.timothy;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ *
+ * @author ASUS ROG Laptop
+ */
+@MessageDriven(activationConfig = {
+    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "jms/myMsgDest"),
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+})
+public class MyBean implements MessageListener {
+    
+    public MyBean() {
+    }
+    
+    @Override
+    public void onMessage(Message message) {
+        TextMessage txtMsg = (TextMessage)message;
+        try {
+            System.out.println(txtMsg.getText());
+        } catch (JMSException ex) {
+            Logger.getLogger(MyBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
---
+++ DemoMDB-war/src/java/MyServlet.java
@@ -0,0 +1,52 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.annotation.Resource;
+import javax.inject.Inject;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/MyServlet"})
+public class MyServlet extends HttpServlet {
+    @Resource(mappedName = "jms/myMsgDest")
+    private Queue myMsgDest;
+    
+    @Resource(mappedName = "jms/myMsg")
+    private ConnectionFactory connectionFactory;
+
+    
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        String str = request.getParameter("txtMsg");
+        PrintWriter pw = response.getWriter();
+        pw.println("<h1>Your messsage is sent</h1>");
+        sendJMSMessageToMyMsgDest(str);
+    }
+
+    private void sendJMSMessageToMyMsgDest(String messageData) {
+        Connection connection;
+        try{
+            connection = connectionFactory.createConnection();
+            Session session = connection.createSession();
+            MessageProducer msgProducer = session.createProducer(myMsgDest);
+        
+            TextMessage tm = session.createTextMessage();
+            tm.setText(messageData);
+            
+            msgProducer.send(tm);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+}
